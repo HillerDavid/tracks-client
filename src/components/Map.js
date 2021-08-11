eeb6f0b2bc7994dad6de77c7a6ec7331db9efd1c
HillerDavid <raiotech@gmail.com>
2021-08-11 00:15:41 -0400

    Created Map component. Created TrackScreen.

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,13 +8,13 @@ const Map = () => {
     for (let i = 0; i < 20; i++) {
         if (i % 2 === 0) {
             points.push({
-                latitude: 37.33233 + i / 1000,
-                longitude: -122.03121 + i / 100,
+                latitude:  28.63252773101975  + i / 1000,
+                longitude: -81.23723920414484 + i / 100,
             });
         } else {
             points.push({
-                latitude: 37.33233 - i / 2000,
-                longitude: -122.03121 + i / 100,
+                latitude:  28.63252773101975  - i / 2000,
+                longitude: -81.23723920414484 + i / 100,
             });
         }
     }
@@ -23,8 +23,8 @@ const Map = () => {
         <MapView
             style={styles.map}
             initialRegion={{
-                latitude: 37.33233,
-                longitude: -122.03121,
+                latitude:  28.63252773101975 ,
+                longitude: -81.23723920414484,
                 latitudeDelta: 0.01,
                 longitudeDelta: 0.01
             }}
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,13 +1,43 @@
-import React from 'react';
+// import '../_mockLocation';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, SafeAreaView, StatusBar } from 'react-native';
 import { Text } from 'react-native-elements';
 import Map from '../components/Map';
+import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null);
+
+    const startWatching = async () => {
+        try {
+            const { granted } = await requestForegroundPermissionsAsync();
+
+            if (!granted) {
+                throw new Error('Location permission not granted');
+            }
+
+            await watchPositionAsync({
+                accuracy: Accuracy.BestForNavigation,
+                timeInterval: 1000,
+                distanceInterval: 10
+            }, (location) => {
+               // console.log('Location updated', location);               
+            });
+
+        } catch (err) {
+            setErr(err);
+        }
+    };
+
+    useEffect(() => {
+        startWatching();
+    }, []);
+
     return (
         <SafeAreaView style={styles.container}>
             <Text h2>Create a Track</Text>
             <Map />
+            {err && <Text>Please enable location permission</Text>}
         </SafeAreaView>
     );
 };
